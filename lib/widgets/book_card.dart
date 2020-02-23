76a312886808d6445d1fabab13757d422b265161
motschel123 <marcel.schoeckel@gmail.com>
2020-02-23 22:43:25 +0100

    "Added TODO"

--- lib/widgets/book_card.dart
+++ lib/widgets/book_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:digital_local_library/data/book.dart';
 
-
 class BookCard extends Card {
   BookCard(this.book);
 
@@ -9,22 +8,27 @@ class BookCard extends Card {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shape: ContinuousRectangleBorder(),
-      child: GestureDetector(
-        child: Container(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxHeight: 400,
-            ),
-            child: SizedBox.expand(
-              child: Image(image: NetworkImage(book.imagePath)),
+    return GestureDetector(
+      onTap: () {
+        // TODO: open bookdetailscreen
+      },
+      child: Card(
+        elevation: 1,
+        shape: ContinuousRectangleBorder(),
+        child: GestureDetector(
+          child: Container(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxHeight: 400,
+              ),
+              child: SizedBox.expand(
+                child: Image(image: NetworkImage(book.imagePath)),
+              ),
             ),
           ),
         ),
+        margin: EdgeInsets.all(10),
       ),
-      margin: EdgeInsets.all(10),
     );
   }
 }
