67f58984e8294ad9206ee4b71493c665c1c786c4
motschel123 <marcel.schoeckel@gmail.com>
2020-02-29 14:28:29 +0100

    "Working new bookfeed layout + loading animation"

--- lib/widgets/book_card.dart
+++ lib/widgets/book_card.dart
@@ -2,87 +2,81 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:digital_local_library/data/book.dart';
 
-class BookCard implements ExpansionPanelRadio{
-    final Book book;
+class BookCard implements ExpansionPanelRadio {
+  final Book book;
 
-    ExpansionPanelHeaderBuilder _headerBuilder;
-    Widget _body;
-    bool _isExpanded;
-    bool _canTapOnHeader;
-     Object _value;
+  ExpansionPanelHeaderBuilder _headerBuilder;
+  Widget _body;
+  bool _isExpanded;
+  bool _canTapOnHeader;
+  Object _value;
 
-    BookCard({@required this.book}) {
-        _canTapOnHeader = true;
-        _isExpanded = false;
-        _headerBuilder = (BuildContext context, bool isExpanded) {};
-        _value = book;
-        _body = Container(
+  BookCard({@required this.book}) {
+    _canTapOnHeader = true;
+    _isExpanded = false;
+    _body = Text("Von Marcel Schöckel");
+    _value = book;
+    _headerBuilder = (BuildContext context, bool isExpaned) => isExpanded
+        ? null
+        : Container(
             height: 120.0,
-            margin: EdgeInsets.symmetric(
-                vertical: 8.0,
-                horizontal: 16.0
-            ),
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
             child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                    Container(
-                        alignment: FractionalOffset.centerLeft,
-                        child: CachedNetworkImage(
-                            placeholder: (context, url) => CircularProgressIndicator(),
-                            errorWidget: (context, url, error) => Icon(Icons.error),
-                            imageUrl: book.imagePath,
-                        ),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  alignment: FractionalOffset.centerLeft,
+                  child: CachedNetworkImage(
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    imageUrl: book.imagePath,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 120.0,
+                    margin: EdgeInsets.only(
+                      left: 16.0,
                     ),
-                    Expanded(
-                        child: Container(
-                            height: 120.0,
-                            margin: EdgeInsets.only(
-                                left: 16.0,
-                            ),
-                            child: Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: <Widget>[
-                                    Text(
-                                        book.title,
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            fontSize: 18.0,
-                                            fontWeight: FontWeight.bold,
-                                        ),
-                                    ),
-                                    Text(
-                                        book.author,
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            color: Colors.black54,
-                                        ),
-                                    ),
-                                ],
-                            ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Text(
+                          book.title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                        Text(
+                          book.author,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
                     ),
-                ],
+                  ),
+                ),
+              ],
             ),
-        );
-    }
+          );
+  }
 
   @override
-  // TODO: implement body
   Widget get body => _body;
 
   @override
-  // TODO: implement canTapOnHeader
   bool get canTapOnHeader => _canTapOnHeader;
 
   @override
-  // TODO: implement headerBuilder
   get headerBuilder => _headerBuilder;
 
   @override
-  // TODO: implement isExpanded
   bool get isExpanded => _isExpanded;
 
   @override
-  // TODO: implement value
   Object get value => _value;
 }
--- lib/widgets/books_feed.dart
+++ lib/widgets/books_feed.dart
@@ -5,55 +5,67 @@ import 'package:digital_local_library/data/book.dart';
 import 'package:digital_local_library/models/books_database_model.dart';
 import 'package:digital_local_library/widgets/book_card.dart';
 
-
 class BooksFeed extends StatelessWidget {
-    final String _searchText;
+  final String _searchText;
 
-    BooksFeed (this._searchText);
+  BooksFeed(this._searchText);
 
-    Widget _createExpansionPanelList (BooksDatabaseModel booksModel) {
-        if (booksModel.books.isEmpty) {
-            return SingleChildScrollView(
-                child:  Center(child: Text("Pull down to refresh!"),),
-            );
-        }
-        return SingleChildScrollView(
-            child: Container (child: ExpansionPanelList.radio(
-            children: List<ExpansionPanelRadio>
-                .generate(
-                    booksModel.books.length, 
+  Widget _createExpansionPanelList(BooksDatabaseModel booksModel) {
+    if (booksModel.books.isEmpty) {
+      return ListView(
+        children: <Widget>[
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 50.0),
+              child: Text(
+                "Pull down to refresh!",
+                textScaleFactor: 1.8,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+    return ListView(children: <Widget>[
+        SingleChildScrollView(
+            child: ExpansionPanelList.radio(
+                children: List<ExpansionPanelRadio>.generate(
+                    booksModel.books.length,
                     (int index) {
                         Book tempBook = booksModel.books[index];
-                if (_searchBookAgainstString(tempBook, _searchText)) {
-                    return BookCard(book: tempBook);
-                } else
-                    return null;
+                        if (_searchBookAgainstString(tempBook, _searchText)) {
+                            return BookCard(book: tempBook);
+                        } else  {
+                            return null;
+                        }
                     },
                 ),
-        )));
-    }
+            ),
+        ),
+    ]);
+  }
 
-    @override
-    Widget build (BuildContext context) {
-        return ScopedModelDescendant<BooksDatabaseModel>(
-            rebuildOnChange: true,
-            builder: (context, child, booksModel) {
-                return RefreshIndicator(
-                    child: _createExpansionPanelList(booksModel),
-                    onRefresh: () {
-                        booksModel.updateBooks();
-                        return Future<void>(() {});
-                    },
-                );
-            },
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModelDescendant<BooksDatabaseModel>(
+      rebuildOnChange: true,
+      builder: (context, child, booksModel) {
+        return RefreshIndicator(
+          child: _createExpansionPanelList(booksModel),
+          onRefresh: () {
+            booksModel.updateBooks();
+            return Future<void>(() {});
+          },
         );
-    }
+      },
+    );
+  }
 
-    bool _searchBookAgainstString (Book book, String searchText) {
-        if (searchText.isEmpty ||
-            (book.title + " " + book.author).toLowerCase().contains(searchText)) {
-            return true;
-        }
-        return false;
+  bool _searchBookAgainstString(Book book, String searchText) {
+    if (searchText.isEmpty ||
+        (book.title + " " + book.author).toLowerCase().contains(searchText)) {
+      return true;
     }
+    return false;
+  }
 }
