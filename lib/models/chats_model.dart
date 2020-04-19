e31702839881c4e569c40bb33dc6019f33c3a415
motschel123 <marcel.schoeckel@gmail.com>
2020-04-19 18:58:41 +0200

    "added firebase interface moved some backend logic"

--- lib/models/chats_model.dart
+++ lib/models/chats_model.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:digital_local_library/data/chat.dart';
+import 'package:digital_local_library/firebase_interfaces/chats.dart';
 import 'package:digital_local_library/sign_in/auth.dart';
 import 'package:digital_local_library/sign_in/user.dart';
 import 'package:digital_local_library/sqlite_db/chat_database_provider.dart';
@@ -8,34 +9,18 @@ import 'package:scoped_model/scoped_model.dart';
 
 class ChatsModel extends Model {
   final Future<CurrentUser> currentUser;
+  ChatsInterface _chatsInterface;
 
   List<Chat> _chats = [];
   List<Chat> get chats => _chats;
 
   ChatsModel({@required this.currentUser}) {
-    currentUser.then((currentUser) {
-      Firestore.instance.collection('users').document(currentUser.uid).collection('userChats').snapshots().map((querySnap) {
-        List<Chat> chatsList = <Chat>[];
-        querySnap.documents.forEach((docSnap) {
-          if(docSnap.exists){
-            chatsList.add(Chat.fromMap({
-            ChatDatabaseProvider.COLUMN_CHAT_DOCUMENT_ID: docSnap.data[ChatDatabaseProvider.COLUMN_CHAT_DOCUMENT_ID],
-            ChatDatabaseProvider.COLUMN_PEERNAME: docSnap.data[ChatDatabaseProvider.COLUMN_PEERNAME],
-            ChatDatabaseProvider.COLUMN_PEER_AVATAR_URL: docSnap.data[ChatDatabaseProvider.COLUMN_PEER_AVATAR_URL],
-          }));
-          }
-        });
-        return chatsList;
-      }).listen((chatsList) {
+    _chatsInterface = ChatsInterface(currentUser: currentUser);
+    _chatsInterface.getChats.then((Stream<List<Chat>> chatsListStream) {
+      chatsListStream.listen((chatsList) {
         _chats = chatsList;
         notifyListeners();
       });
-    });
-  }
-
-  Future<Chat> chatWithPeer(String peerName) async {
-    currentUser.then<CurrentUser>((currentUser) {
-      
     });
   }
 }
